f9171de9137ca5b7a656745ee9221959d76a1f81
Kosarewicz Michal <kosarewicz.michal@gmail.com>
2022-08-07 16:45:29 +0200

    zmiana c

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        String b = "zmiana a";
-        System.out.println(b);
-        System.out.println("Hello "+ b);
+        String c = "zmiana a";
+        System.out.println(c);
+        System.out.println("Hello "+ c);
         
     }
 }
